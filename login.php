af9dc455c14f9b14716667bf847ed2fd53f7290f
imackobayashi <jazzbass.s.k@gmail.com>
2024-07-26 18:58:27 +0900

    last commit

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ session_start();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
-    <script src="script4.js" defer></script>
+    <script src="script5.js" defer></script>
     <title>Index4</title>
 </head>
 
@@ -21,10 +21,7 @@ session_start();
         <h1>jsでFormからデータ取得</h1>
         <form action="" name="contact">
             <ul>
-                <li><input id="postcode" name="postcode" type="number" placeholder="郵便番号７桁を入力してください！！"></li>
-                <li><input id="prefecture" name="prefecture" type="text" placeholder="都道府県名"></li>
-                <li><input id="city" name="city" type="text" placeholder="市区町村名"></li>
-                <li><input id="town" name="town" type="text" placeholder="町名他"></li>
+                <li><input type="password" name="password" class="form-control" required /></li>
             </ul>
         </form>
 
